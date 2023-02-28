a92b3a0126701539d49ead63e4a1d82d4ae2e061
Christian Pascua <cristianpascuafdez@gmail.com>
2023-02-28 13:53:23 +0100

    Added Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import footer_logo from '../assets/footer-logo.png'
+
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <div className="footer__container">
+        <img src={footer_logo} alt="Little lemon logo" />
+        <div className="footer__copyright">
+          <p>© 2020 Little Lemon</p>
+        </div>
+      </div>
+    </footer>
   )
 }
 
